cc6598e7c703f010914b42cf04aab6c0b016e730
somadritadey <deysomadrita69@gmail.com>
2026-06-23 12:16:01 +0530

    fix: remove duplicate package declaration in DocMindApplication

--- backend/src/main/java/com/accenture/intern/docmind/DocMindApplication.java
+++ backend/src/main/java/com/accenture/intern/docmind/DocMindApplication.java
@@ -1,7 +1,5 @@
 package com.accenture.intern.docmind;
 
-package com.accenture.intern.docmind;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
